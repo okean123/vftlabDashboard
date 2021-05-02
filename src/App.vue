951706a4a6a3831386d6b05070bea1ad913c507c
okean123 <okean.123@mail.ru>
2021-05-02 19:55:45 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+
+  <div id = "app" :style="cssProps">
+    <HelloWorld />
+  </div>
+</template>
+
+
+<script>
+import HelloWorld from "@/components/main.vue";
+
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      cssProps: {
+        backgroundImage: 'url(' + require('./assets/landscape.png') + ')',
+      }
+    }
+  },
+  methods: {
+  },
+  components: {
+    HelloWorld: HelloWorld
+  },
+  metaInfo: {
+    script: [
+        { src: "https://unpkg.com/axios/dist/axios.min.js", async: true, defer: true },
+        { src: "https://cdn.jsdelivr.net/npm/sscjs@latest/dist/ssc.min.js", async: true, defer: true },
+        { src: "https://code.jquery.com/jquery-3.6.0.min.js", async: true, defer: true }
+    ]
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  min-height: 100%;
+  height: auto !important;
+  height: 100%;
+  background-size:     cover;
+  background-repeat:   no-repeat;
+}
+
+html, body {
+  height: 99%;
+  background-color: cornsilk;
+}
+
+
+</style>
---
+++ src/components/main.vue
@@ -0,0 +1,439 @@
+<template>
+  <div id="main">
+    <img id = "topLogo" alt="VFT Defi Farmer" src="../assets/farmer.png">
+    <h1> {{ msg }}</h1>
+    <br>
+    <div id = "infoBox">
+      <p> Note that you need to install and activate an extension like 'Allow CORS: Access-Control-Allow-Origin' </p>
+      <p> in your browser for the app to work correctly: <a href="https://chrome.google.com/webstore/detail/allow-cors-access-control/lhobafahddgcelffkeicbaginigeejlf">Chrome</a>
+        or <a href="https://addons.mozilla.org/de/firefox/addon/access-control-allow-origin/">Firefox</a> </p>
+    </div>
+    <br>
+    <button @click="updateData"> Update data</button>
+    <br>
+    <br>
+    <div id = "stats">
+      <div>
+        <div id = "tokenStats" class = "statsBox">
+          <p> Token stats:</p>
+          <div>
+            <span class = "tokenText"> Market Cap:</span> <span> {{tokenStats["marketcap"]}} HIVE </span>
+            <br>
+            <span class = "tokenText"> VFT Minted:</span> <span> {{tokenStats["totalSupply"]}} </span>
+            <br>
+            <span class = "tokenText"> VFT Burned:</span> <span> {{tokenStats["burned"]}} </span>
+            <br>
+            <span class = "tokenText"> %Burned:</span> <span> {{formatNumber(tokenStats["burned"] * 100 /
+              tokenStats["totalSupply"])}}% </span>
+            <br>
+            <span class = "tokenText"> New VFT/Day:</span> <span> {{tokenStats["inflationRate"]}} </span>
+            <br>
+            <span class = "tokenText"> VFT Price:</span> <span> {{ Number.parseFloat(tokenPrices["VFT"]).toFixed(3)}} HIVE </span>
+            <br>
+          </div>
+        </div> <!-- End token stats-->
+
+        <div  id = "inflationStats" class = "statsBox">
+          <p> Daily VFT inflation: </p>
+          <div>
+            <span class = "tokenText"> HIVE:</span> <span> {{ tokenStats["inflationRate"] * inflationStats['SWAP.HIVE'] / 100}} ({{inflationStats['SWAP.HIVE']}}%) </span>
+            <br>
+            <span class = "tokenText"> VIBES:</span> <span> {{ tokenStats["inflationRate"] * inflationStats['VIBES'] / 100}} ({{inflationStats['VIBES']}}%) </span>
+            <br>
+            <span class = "tokenText"> VFT:</span> <span> {{ tokenStats["inflationRate"] * inflationStats['VFT'] / 100}} ({{inflationStats['VFT']}}%) </span>
+            <br>
+            <span class = "tokenText"> LEO:</span> <span> {{ tokenStats["inflationRate"] * inflationStats['LEO'] / 100}} ({{inflationStats['LEO']}}%) </span>
+            <br>
+            <span class = "tokenText"> DEC:</span> <span> {{ tokenStats["inflationRate"] * inflationStats['DEC'] / 100}} ({{inflationStats['DEC']}}%) </span>
+            <br>
+            <span class = "tokenText"> STARBITS:</span> <span> {{ tokenStats["inflationRate"] * inflationStats['STARBITS'] / 100}} ({{inflationStats['STARBITS']}}%) </span>
+            <br>
+            <br>
+          </div>
+        </div> <!-- End inflation stats-->
+
+        <div id = "depositStats" class = "statsBox">
+          <p> Total deposited funds: </p>
+          <div>
+            <span class = "tokenText"> HIVE (currently wrong):</span> <span> {{tokenAmounts['SWAP.HIVE']}} </span>
+            <br>
+            <span class = "tokenText"> VIBES:</span> <span> {{tokenAmounts['VIBES']}} </span>
+            <br>
+            <span class = "tokenText"> VFT:</span> <span> {{tokenAmounts['VFT']}} </span>
+            <br>
+            <span class = "tokenText"> LEO:</span> <span> {{tokenAmounts['LEO']}} </span>
+            <br>
+            <span class = "tokenText"> DEC:</span> <span> {{tokenAmounts['DEC']}} </span>
+            <br>
+            <span class = "tokenText"> STARBITS:</span> <span> {{tokenAmounts['STARBITS']}} </span>
+            <br>
+          </div>
+        </div> <!-- End deposit stats-->
+
+        <div id = "aprStats" class = "statsBox">
+          <p> Current APR: </p>
+          <div>
+            <span class = "tokenText"> HIVE:</span> <span> {{formatNumber(aprStats['SWAP.HIVE'])}}% </span>
+            <br>
+            <span class = "tokenText"> VIBES:</span> <span> {{formatNumber(aprStats['VIBES'])}}% </span>
+            <br>
+            <span class = "tokenText"> VFT:</span> <span> {{formatNumber(aprStats['VFT'])}}% </span>
+            <br>
+            <span class = "tokenText"> LEO:</span> <span> {{formatNumber(aprStats['LEO'])}}% </span>
+            <br>
+            <span class = "tokenText"> DEC:</span> <span> {{formatNumber(aprStats['DEC'])}}% </span>
+            <br>
+            <span class = "tokenText"> STARBITS:</span> <span> {{formatNumber(aprStats['STARBITS'])}}% </span>
+            <br>
+          </div>
+        </div> <!-- End deposit stats-->
+      </div> <!-- End stats -->
+    </div>
+  </div>
+</template>
+
+<script>
+import $ from 'jquery'
+
+// Initting vars
+let ssc;
+let msg = "VFTLAB Farming Stats"
+
+// available token pools
+const tokens = ['SWAP.HIVE', 'VIBES', 'STARBITS', 'LEO', 'DEC', 'VFT']
+
+// saves pooled token amounts
+let depositStats = {}
+
+// saves stats about the token
+let tokenStats = {}
+
+// save the inflation distribution
+let inflationStats = {}
+
+// save apr rates
+let aprStats = {}
+
+// save prices of tokens in hive
+let tokenPrices = {}
+
+$.when(
+    $.getScript( "https://cdn.jsdelivr.net/npm/sscjs@latest/dist/ssc.min.js" ),
+    $.Deferred(function( deferred ){
+      $( deferred.resolve );
+    })
+).done(function(){
+  // eslint-disable-next-line no-undef
+    ssc = new SSC('https://api.hive-engine.com/rpc')
+}).done(function () {
+    init()
+})
+
+
+function init() {
+  updateData()
+}
+
+// Updates tokens deposited to the pools
+// depositStats objects stores the values
+function getDepositedTokens() {
+  let tokenBal = {}
+  ssc.find(
+      'tokens',
+      'balances',
+      {
+        account:  'vftlab'
+      }, (result) => {
+        // eslint-disable-next-line no-undef
+        resolve(result)
+      }).then(function (result) {
+    for (let token of result) {
+      let tokenbal = formatNumber(token.balance)
+      depositStats[token.symbol] = tokenbal
+      tokenBal[token.symbol] = tokenbal
+    }
+    //TODO Add correct Hive value (get from Hive directly, not HE)
+  }).then(function () {
+    getAPRStats() //TODO Fix async shit later
+  })
+  return tokenBal
+}
+
+// get Stats about the VFT token itself
+function getTokenStats() {
+  // VFT supply
+  ssc.findOne(
+      'tokens',
+      'tokens',
+      {
+        symbol:  'VFT'
+        // eslint-disable-next-line no-unused-vars
+      }, () => {
+        // resolve(result)
+      }).then(function (result) {
+        tokenStats["totalSupply"] = formatNumber(result.supply)
+        setMarketCap()
+        })
+  // VFT burned
+  ssc.findOne(
+      'tokens',
+      'balances',
+      {
+        account:  'null',  // tokens in null == token burned
+        symbol: 'VFT'
+        // eslint-disable-next-line no-unused-vars
+      }, (result) => {
+        // resolve(result)
+      }).then(function (result) {
+         tokenStats["burned"] = formatNumber(result.balance)
+    })
+
+  // Get inflation rate
+  $.get('https://vftlab.herokuapp.com/stats/').then(function(result) {
+    if (result) {
+      tokenStats["inflationRate"] = result.data.stats.new_vft_block
+    }
+  });
+}
+
+// sets the market cap in tokenStats["marketcap"]; called in getTokenStats()
+function setMarketCap() {
+  // Market Cap in HIVE using last trading price
+  ssc.findOne(
+      'market',
+      'metrics',
+      {
+        symbol:  'VFT'
+        // eslint-disable-next-line no-unused-vars
+      }, (result) => {
+      }).then(function (result) {
+    // console.log("Price: " + result.lastPrice + " Supply: " + (tokenStats["totalSupply"] - tokenStats["burned"]))
+    tokenStats["marketcap"] = formatNumber(result.lastPrice * (tokenStats["totalSupply"] - tokenStats["burned"]))
+  })
+}
+
+// get the distribution of the inflation
+function getInflationStats() {
+  $.get('https://vftlab.herokuapp.com/getMonedas/').then(function (result) {
+    if (result) {
+      for (let token of result.data.monedas) {
+        inflationStats[token.coind_name] = token.pool
+      }
+    }
+  })
+}
+
+// calculate the aprs for the tokens
+function  getAPRStats() {
+    for (let token of tokens) {
+      let depositsValue = depositStats[token] * tokenPrices[token]
+      let vftYield = inflationStats[token] * (tokenStats["inflationRate"] / 100)
+      let yieldValue = vftYield * tokenPrices["VFT"]
+      let dailyAPR = yieldValue / depositsValue * 100
+      aprStats[token] = dailyAPR * 365
+    }
+}
+
+function getTokenPrices() {
+  for (let token of tokens) {
+    if (token == "SWAP.HIVE") continue;
+    ssc.findOne(
+        'market',
+        'metrics',
+        {
+          symbol:  token
+        }, () => {
+        }).then(function (result) {
+      tokenPrices[token] = result.lastPrice
+    }).then(function () {
+      getDepositedTokens()
+    })
+  }
+}
+
+// Updates all numbers
+function updateData() {
+  getTokenPrices()
+  // getDepositedTokens() Is called in getTokenPrices
+  // getAPRStats() Is called in getDepositedTokens
+  getTokenStats()
+  getInflationStats()
+}
+
+// Creates a dummy object for the token amounts object to pass on page load
+function createTokenAmountsObj() {
+  for (let token of tokens) {
+    depositStats[token] = 0
+  }
+  return depositStats
+}
+
+// Creates a dummy object for the token stats object to pass on page load
+function createTokenStatsObj() {
+  tokenStats["totalSupply"] = 0
+  tokenStats["burned"] = 0
+  tokenStats["marketcap"] = 0
+  tokenStats["inflationRate"] = 1000
+  return tokenStats
+}
+
+function createInflationStatsObj() {
+  inflationStats["VIBES"] = 30
+  inflationStats["VFT"] = 20
+  inflationStats["SWAP.HIVE"] = 20
+  inflationStats["LEO"] = 12.5
+  inflationStats["DEC"] = 12.5
+  inflationStats["STARBITS"] = 5
+  return inflationStats
+}
+
+function createAPRStatsObj() {
+  for (let token of tokens) {
+    aprStats[token] = 0
+  }
+  return aprStats
+}
+
+function createTokenPricesObj() {
+  for (let token of tokens) {
+    tokenPrices[token] = 0
+  }
+  return tokenPrices
+}
+
+// rounds number and adds space after each block of 3 numbers
+function formatNumber(n) {
+  // round to 0 decimal places
+    n = Number.parseFloat(n).toFixed(0)
+  // add space.
+  // n = n.replace(new RegExp("^(\\d{" + (n.length%3?n.length%3:0) + "})(\\d{3})", "g"),
+  //   "$1 $2").replace(/(\d{3})+?/gi, "$1 ").trim();
+  return n
+}
+
+
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      msg: msg,
+      tokenStats: createTokenStatsObj(),
+      inflationStats: createInflationStatsObj(),
+      tokenAmounts: createTokenAmountsObj(),
+      aprStats: createAPRStatsObj(),
+      tokenPrices: createTokenPricesObj()
+    }
+  },
+  methods: {
+    updateData,
+    formatNumber
+  },
+  mounted() {
+    let axiosscript = document.createElement('script')
+    axiosscript.setAttribute('src', 'https://unpkg.com/axios/dist/axios.min.js')
+    document.head.appendChild(axiosscript)
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+html {
+  background-image: url("../assets/landscape.png");
+}
+
+h1{
+  color: black;
+  text-shadow: 1px 1px white;
+}
+
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+#stats {
+  -webkit-text-size-adjust: 100%;
+  font-family: Verdana,sans-serif;
+  line-height: 1.5;
+  color: #000!important;
+  box-sizing: inherit;
+  border-radius: 16px;
+  padding: 5px 0;
+  border: 1px solid black;
+  width: max-content;
+
+  max-height: 90%;
+  margin: auto;
+  overflow: hidden;
+  border-spacing: 5px 0;
+  background: rgba(0, 0, 0, 0.5);
+  font-size: 1.05em;
+  align-items: center;
+  display: flex;
+  justify-content: center;
+}
+
+.statsBox {
+  border: 1px solid red;
+  border-radius: 16px;
+  display: table-cell;
+  padding: 0.2em 16px;
+  font-weight: bold;
+  background: rgba(255, 255, 255, 0.8);
+  margin: 0 auto;
+}
+
+.tokenText {
+  font-size: 1.05em;
+  color: mediumblue;
+}
+
+p {
+  font-weight: bold;
+}
+
+button {
+  align-items: center;
+  background-color: rgb(170, 137, 41);
+  border: 0;
+  border-radius: 16px;
+  box-shadow: 0 0 7px 3px rgba(0,0,0,0.8);
+  color: rgb(255, 255, 255);
+  display: inline-flex;
+  font-weight: 600;
+  height: 50px;
+  outline: 0;
+  padding: 0 24px;
+  transition: background-color 0.2s ease 0s;
+  opacity: 1;
+  margin-top: 5px;
+}
+button:hover {
+  background-color: rgb(170, 100, 10);
+}
+
+#topLogo {
+  margin-top: 5px;
+}
+
+#infoBox {
+  margin: 0 auto;
+  border: 1px solid red;
+  border-radius: 16px;
+  background: rgba(255, 255, 255, 0.8);
+  width: max-content;
+  padding: 5px;
+}
+</style>
